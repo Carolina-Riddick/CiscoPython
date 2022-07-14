809e1e52a2d53e6ec509aa268a75bfdf224d78bc
Carolina Riddick <carolinariddick@gmail.com>
2022-07-14 20:37:09 -0300

    CrearLista

---
+++ CreandoLista.py
@@ -0,0 +1,9 @@
+﻿#Creamos una lista con n cantidad de numeros y los vamos agregando.
+
+def create_list(n):
+    my_list = []
+    for i in range(n):
+        my_list.append(i)
+    return my_list
+
+print(create_list(5))
